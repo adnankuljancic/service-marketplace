f306eb124e9014fb840d09bcc876530bb9cafa5c
Adnan Kuljancic <adnan.kuljancic@outlook.com>
2023-12-12 15:20:47 +0100

    Search bar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import MainNavbar from "./components/MainNavbar";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
     <div className="App">
       <MainNavbar />
+      <SearchBar />
     </div>
   );
-} 
+}
 
 export default App;
