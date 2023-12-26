7ca31eec287902e400a934c7a8b65c8166abde0c
Adnan Kuljancic <adnan.kuljancic@outlook.com>
2023-12-26 19:35:17 +0100

    typo

--- src/components/MainNavbar/MainNavbar.jsx
+++ src/components/MainNavbar/MainNavbar.jsx
@@ -1,6 +1,7 @@
 import Container from "react-bootstrap/Container";
-import Nav from 'react-bootstrap/Nav';
+import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 
 function MainNavbar() {
   return (
@@ -8,13 +9,16 @@ function MainNavbar() {
       <Navbar className="bg-body-tertiary">
         <Container>
           <Navbar.Brand href="/">ServiceMarketplace</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav"/>
-          <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse
+            className="justify-content-end"
+            id="basic-navbar-nav"
+          >
             <Nav className="ms-auto">
               <Nav.Link to="/">Home</Nav.Link>
-              <Nav.Link href="#services">Services</Nav.Link>
+              <Nav.Link to="">Services</Nav.Link>
               <Nav.Link to="/login">Log in</Nav.Link>
-              <Nav.Link href="#services">Sign up</Nav.Link>
+              <Nav.Link to="">Sign up</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
