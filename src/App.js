13548a66b3e4b61f1f7cb004d4072b1912347fdb
Adnan Kuljancic <adnan.kuljancic@outlook.com>
2024-01-02 16:29:22 +0100

    Routing NavBar resolved

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import MainNavbar from "./components/MainNavbar/MainNavbar";
-import SearchBarWrapper from "./components/SearchBarWrapper/SearchBarWrapper";
 import Homepage from "./pages/Home/Home";
 import LoginPage from "./pages/Login/Login";
 
-const router = createBrowserRouter([
-  { path: "/", element: <Homepage /> }, 
-  { path: "/login", element: <LoginPage/>}]);
-
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <MainNavbar />
-      <RouterProvider router={router} />
-    </div>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/MainNavbar/MainNavbar.jsx
+++ src/components/MainNavbar/MainNavbar.jsx
@@ -1,28 +1,37 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom"
+
+
 
 function MainNavbar() {
   return (
     <>
-      <Navbar className="bg-body-tertiary">
-        <Container>
-          <Navbar.Brand href="/">ServiceMarketplace</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse
-            className="justify-content-end"
-            id="basic-navbar-nav"
-          >
-            <Nav className="ms-auto">
-              <Nav.Link to="/">Home</Nav.Link>
-              <Nav.Link to="">Services</Nav.Link>
-              <Nav.Link to="/login">Log in</Nav.Link>
-              <Nav.Link to="">Sign up</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <Navbar className="bg-body-tertiary" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          ServiceMarketplace
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/">
+              Home
+            </Nav.Link>
+            <Nav.Link as={Link} to="/">
+              Services
+            </Nav.Link>
+            <Nav.Link as={Link} to="/login">
+              Log in
+            </Nav.Link>
+            <Nav.Link as={Link} to="/">
+              Sign up
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     </>
   );
 }
